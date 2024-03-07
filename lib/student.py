97f3e84c7f4e5a44f265451040e5443d0cb84dc0
estherwanjiku007 <estherwanjiqu@gmail.com>
2024-03-07 08:48:18 +0300

    ain

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 
 from user import User
-
+knowledge=[]
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        
+    def learn(self,a_string):
+        knowledge.append(a_string)
+        self.knowledge=knowledge
+
+        
